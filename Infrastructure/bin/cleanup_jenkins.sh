a6a774eeeb476781ecd2f6a77ae0a359a4d7541b
Carlos M. Cornejo <cmcornejo@atsistemas.com>
2018-06-29 15:14:09 +0200

    :Jenkins

---
+++ Infrastructure/bin/cleanup_jenkins.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Setup Jenkins Project
+if [ "$#" -ne 1 ]; then
+    echo "Usage:"
+    echo "  $0 GUID"
+    exit 1
+fi
+
+GUID=$1
+echo "Setting up Nexus in project $GUID-jenkins"
+
+oc project ${GUID}-jenkins
+oc delete all --all
+oc delete cm jenkins-agent
+oc delete pvc jenkins
